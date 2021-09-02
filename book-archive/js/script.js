c2045dc8e770ae06314ede67f59765dde8782b1e
masudtalukdar <masudtalukdar.cn@gmail.com>
2021-09-02 23:27:31 +0800

    first commit

--- book-archive/js/script.js
+++ book-archive/js/script.js
@@ -1,15 +1,17 @@
+// onclick function for search button
 const bookArchive = (text) => {
   const books = document.getElementById('search-field').value;
-  const url = `http://openlibrary.org/search.json?q=${text}`;
-  //   const url = `http://openlibrary.org/search.json?q=${searchText}`;
 
+  const url = `http://openlibrary.org/search.json?q=${text}`;
+
+  // fetching url
   fetch(url)
     .then((res) => res.json())
     .then((data) => displayBooks(data.docs));
 };
-// bookArchive();
+// function for get data and displaying to ui
 const displayBooks = (books) => {
-  console.log(books.length);
+  // console.log(books.length);
 
   const searchResult = document.getElementById('search-result');
   const count = document.getElementById('count');
@@ -23,13 +25,14 @@ const displayBooks = (books) => {
         <div class="card h-100">
             <div class='p-2'>
              <img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top
+            
             " alt="...">
             </div>
             <div id='card-font' class="card-body">
                 <h5 class="book-name">${book?.title}</h5>
-               <h4 class="author-name">Writer: ${book?.author_name[0]}</h4>
-               <h4 class="publisher-name">Publisher: ${book?.publisher[0]}</h4>
-               <h4 class="published-date"> published Year: ${book?.publish_year}</h4>
+               <h4 class="author-name">Writer: ${book?.author_name}</h4>
+               <h4 class="published-date"> published Year: ${book?.first_publish_year}</h4>
+               <h4 class="publisher-name">Publisher: ${book?.publisher}</h4>
             </div>
         </div>
         `;
